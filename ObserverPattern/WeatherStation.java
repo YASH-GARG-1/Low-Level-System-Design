da5d248b87136c60eeea0fe8f2ac840065eb4c3b
YASH-GARG-1 <yashgarg5000@gmail.com>
2022-11-16 23:30:15 +0530

    Adding HeatIndexDisplay in the Observer Pattern.

--- ObserverPattern/WeatherStation.java
+++ ObserverPattern/WeatherStation.java
@@ -8,6 +8,7 @@ public class WeatherStation {
         CurrentConditionDisplay currentDisplay = new CurrentConditionDisplay(weatherData);
         StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
         ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        HeatIndexDisplay heatIndex = new HeatIndexDisplay(weatherData);
 
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(82, 70, 29.2f);
